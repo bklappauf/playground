0b9365f4efe097765d408f88673f36cdfa0561c1
Bruce Klappauf <bklappauf@BruceMBP.local>
2013-12-31 16:51:52 -0600

    got core pt picking to work

--- overlay.py
+++ overlay.py
@@ -4,6 +4,7 @@
 """
 # Std lib imports
 from random import random
+import sys
 
 # ETS imports
 import numpy as np
@@ -34,7 +35,29 @@ recalc = Action(name = "Recalculate",
 ################################################################################
 # Custom Tools
 ################################################################################
+class PickTool(BaseTool):
+    ''' select core sample pts and move vertical line there
 
+    '''
+    vert_line_component_index = Int(3)
+
+    def normal_left_down(self,event):
+        print 'in pick nld'
+        core = self.component
+        mouse = (event.x, event.y)
+        point = core.map_data(mouse)
+        hitpt = core.hittest(mouse)
+        print 'ms,pt=',mouse, point, hitpt
+        #import ipdb; ipdb.set_trace()
+        if hitpt:
+            nearest_x, nearest_y = core.map_data(hitpt)
+            print nearest_x, nearest_y
+            vline = core.container.components[3]
+            data = vline.value.get_data()
+            print vline, data
+            vline.value.set_data([nearest_x for value in data])
+        print '\n ############  pick done\n'
+        sys.stdout.flush()
 
 class TraceTool(BaseTool):
     ''' Allows mouse update of impoundment boundary trace
@@ -113,49 +136,53 @@ class TraceTool(BaseTool):
             self.last_index = current_index
             self.last_y = newy
 
-    def normal_left_down(self, event):
-        print '\n ############  nleft_down'
-        # don't know how to access this plot by name.
-        corepts = self.component.components[2]
-        print corepts
-        corexy = corepts.map_screen((0,-50))
-        pxy = self.component.map_screen((0,-50))
-        print corexy, pxy
-        print corexy-pxy
-        (mx,my) = (event.x,event.y)
-        xdata,ydata = corepts.index.get_data(), corepts.value.get_data()
-        xydata = np.vstack((xdata,ydata))
-        screen = corepts.map_screen( xydata.T)
-        diff = screen-(mx,my)
-        dist_array = np.sum(diff**2, axis=1)
-        index = np.argmin(dist_array)
-        x,y = xdata[index], ydata[index]
-        print index,screen, (mx,my), [x,y]
-        vline = self.component.components[3]
-        data = vline.value.get_data()
-        vline.value.set_data([x for value in data])
-#        #(mx,my) = corepts.map_data((event.x,event.y))
-#        nearest = corepts.get_closest_point((mx,my), self.PROXIMITY)
-#        print nearest
-#        print corepts.map_data((event.x,event.y))
-#        if nearest:
-#            [x,y,d] = nearest
-#            newx,newy = corepts.map_data( (x, y))
-#            print (event.x,event.y), [x,y],newx,newy
-#            vline = self.component.components[3]
-#            print vline
-#            data = vline.value.get_data()
-#            print data
-#            vline.value.set_data([newx for value in data])
+#    def normal_left_down(self, event):
+#        print '\n ############  nleft_down'
+#        # don't know how to access this plot by name.
+#        corepts = self.component.components[2]
+#        print corepts
+#        corexy = corepts.map_screen((0,-50))
+#        pxy = self.component.map_screen((0,-50))
+#        print corexy, pxy
+#        print corexy-pxy
+#        (mx,my) = (event.x,event.y)
+#        xdata,ydata = corepts.index.get_data(), corepts.value.get_data()
+#        xydata = np.vstack((xdata,ydata))
+#        screen = corepts.map_screen( xydata.T)
+#        diff = screen-(mx,my)
+#        dist_array = np.sum(diff**2, axis=1)
+#        index = np.argmin(dist_array)
+#        x,y = xdata[index], ydata[index]
+#        print index,screen, (mx,my), [x,y]
+#        vline = self.component.components[3]
+#        data = vline.value.get_data()
+#        vline.value.set_data([x for value in data])
+#        import ipdb; ipdb.set_trace()
 #
+#        print '\n ############  nleft_down done\n'
+#        sys.stdout.flush()
+##        #(mx,my) = corepts.map_data((event.x,event.y))
+##        nearest = corepts.get_closest_point((mx,my), self.PROXIMITY)
+##        print nearest
+##        print corepts.map_data((event.x,event.y))
+##        if nearest:
+##            [x,y,d] = nearest
+##            newx,newy = corepts.map_data( (x, y))
+##            print (event.x,event.y), [x,y],newx,newy
+##            vline = self.component.components[3]
+##            print vline
+##            data = vline.value.get_data()
+##            print data
+##            vline.value.set_data([newx for value in data])
+##
+##
+#        #xdata,ydata = self.data_xy
+#        #xydata = vstack((xdata,ydata))
+#        #screen = self.component.map_screen( xydata.T)
+#        #diff = screen-(event.x,event.y)
+#        #dist_array = sum(diff**2, axis=1)
+#        #index = argmin(dist_array)
 #
-        #xdata,ydata = self.data_xy
-        #xydata = vstack((xdata,ydata))
-        #screen = self.component.map_screen( xydata.T)
-        #diff = screen-(event.x,event.y)
-        #dist_array = sum(diff**2, axis=1)
-        #index = argmin(dist_array)
-
 
 
 class AppWindow(ModelView):
@@ -234,6 +261,7 @@ class AppWindow(ModelView):
         plots.overlays.append(legend)
 
         # Add tools
+        scatter.tools.append(PickTool(scatter))
         plots.tools.append(TraceTool(plots))
         plots.tools.append(PanTool(plots))
         plots.tools.append(ZoomTool(plots))
